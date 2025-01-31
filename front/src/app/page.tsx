7deff9e54a9b259fbbbd98aa3d6499cd5b7a0a71
jhphon0730 <eeung025@gmail.com>
2025-01-31 13:43:12 +0900

    feat : 메인 페이지 접근 시에 페이지 이동 추가

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,19 @@
-export default function Home() {
-  return (
-    <div className="text-red-500">
-		</div>
-  );
-}
+"use client";
+
+import React from 'react';
+
+import { useRouter } from 'next/navigation'
+
+const MainPage = () => {
+	const router = useRouter();
+
+	React.useEffect(() => {
+		router.push('/dashboard');
+	}, []);
+
+	return (
+		<div></div>
+	);
+};
+
+export default MainPage;
