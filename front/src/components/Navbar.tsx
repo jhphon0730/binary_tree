682a851e45029cf72b8c87e81dd352c002ed89f5
jhphon0730 <eeung025@gmail.com>
2025-02-03 13:24:32 +0900

    feat : 프로필 이미지가 보이도록 수정

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -3,7 +3,8 @@
 import React from 'react'
 import Swal from 'sweetalert2'
 import Cookies from 'js-cookie'
-import { User, UserRoundPlusIcon } from 'lucide-react'
+import { User } from 'lucide-react'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 import {
@@ -46,20 +47,23 @@ const Navbar = () => {
       <SidebarTrigger className="md:hidden" />
       <h1 className="text-xl font-semibold md:hidden">커플 다이어리</h1>
       <div className='flex items-center'>
-        <Button className='me-4'>
-          <UserRoundPlusIcon />
-        </Button>
-        <h1 className='me-2'>{authStore.user?.name}</h1>
-        <DropdownMenu>
+				{authStore.user && <DropdownMenu>
           <DropdownMenuTrigger asChild className='cursor-pointer'>
-            <User className="h-5 w-5" />
+						<Image 
+							src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${authStore.user.profile_image_file}`}
+							alt={authStore.user.name}
+							width={40}
+							height={40}
+							className='rounded-full'
+						/>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            <DropdownMenuItem>프로필</DropdownMenuItem>
+						{/*title*/}
+						<DropdownMenuItem className="font-bold">{authStore.user.name}</DropdownMenuItem>
             <DropdownMenuItem>설정</DropdownMenuItem>
             <DropdownMenuItem onClick={signOutHandler}>로그아웃</DropdownMenuItem>
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu> }
       </div>
     </nav>
 	)
