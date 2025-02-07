a6f8db1256ef163098d03a24a80cc42b616bc986
jhphon0730 <eeung025@gmail.com>
2025-02-07 15:20:36 +0900

    feat : 초대코드 생성 시에 작동해야 하는 로직 beforeCreate 추가

--- internal/model/coupleInvitationModel.go
+++ internal/model/coupleInvitationModel.go
@@ -8,7 +8,12 @@ type CoupleInvitation struct {
 	SenderID   uint   `json:"sender_id" gorm:"not null"` // 초대한 유저
 	ReceiverID *uint  `json:"receiver_id,omitempty"`     // 초대받은 유저
 	InviteCode string `json:"invite_code" gorm:"unique;not null"`
-	Status     string `json:"status" gorm:"default:'pending'"` // "pending", "accepted", "rejected"
+	Status     string `json:"status" gorm:"default:'pending'"` // "pending", "accepted"
+}
+
+func (coupleInvitation *CoupleInvitation) BeforeCreate(tx *gorm.DB) (err error) {
+	coupleInvitation.Status = "pending"
+	return
 }
 
 func FindMyCoupleByUserID(DB *gorm.DB, partnerID uint) (User, error) {
