b8067cef779bcf10ddf1a1214355e94967b74a15
jhphon0730 <eeung025@gmail.com>
2025-02-11 10:41:22 +0900

    feat : 다이어리 모델 추가

---
+++ internal/model/diaryModel.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Diary struct {
+	gorm.Model
+
+	CoupleID uint `json:"couple_id" gorm:"not null" binding:"required" validate:"required"`  // 어떤 커플의 일기인지
+	AuthorID uint `json:"author_id" gorm:"not null" binding:"required" validate:"required"`  // 작성자 ID
+
+	Couple Couple `gorm:"foreignKey:CoupleID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Author User   `gorm:"foreignKey:AuthorID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+
+	Title     string    `json:"title" gorm:"type:varchar(255);not null" binding:"required"`
+	Content   string    `json:"content" gorm:"type:text;not null" binding:"required"`
+	Emotion   string    `json:"emotion" gorm:"type:varchar(50)"`                              // 감정 상태 (선택 사항)
+	DiaryDate time.Time `json:"diary_date" gorm:"not null" binding:"required"`                // 일기 날짜 (작성일과 다를 수 있음)
+}
