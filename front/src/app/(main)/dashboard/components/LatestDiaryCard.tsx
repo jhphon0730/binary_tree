9b4390dc44104908d4e21e9aaf401150626706d4
jhphon0730 <eeung025@gmail.com>
2025-02-13 12:33:48 +0900

    feat : 대시보드에서 다이어리 페이지로 이동하는 기능 추가

--- front/src/app/(main)/dashboard/components/LatestDiaryCard.tsx
+++ front/src/app/(main)/dashboard/components/LatestDiaryCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { format } from "date-fns"
 import { ko } from "date-fns/locale"
 
@@ -27,15 +28,21 @@ const LatestDiaryCard = ({ latestDiary }: RecentDiariesCardProps) => {
 				{
 					latestDiary.ID !== 0 && (
 						<div>
-							<p className="text-sm text-gray-500 mb-3">최근에 작성한 일기입니다.</p>
+							<p className="text-sm text-gray-500 mb-2">최근에 작성한 일기입니다.</p>
 							<p className="font-semibold">{latestDiary.title}</p>
 							<p className="text-sm text-gray-500">작성일: {format(new Date(latestDiary.CreatedAt), "PPP", { locale: ko })}</p>
+							<p className="text-sm text-gray-500">작성 기준일: {format(new Date(latestDiary.diary_date), "PPP", { locale: ko })}</p>
+							<p className="text-sm text-gray-500">이미지: {latestDiary.images.length}개</p>
 						</div>
 					)
 				}
       </CardContent>
       <CardFooter>
-        <Button variant="outline">일기 쓰기</Button>
+        <Button variant="outline">
+					<Link href="/dashboard/diary">
+						다이어리 목록 보기
+					</Link>
+				</Button>
       </CardFooter>
     </Card>
   )
