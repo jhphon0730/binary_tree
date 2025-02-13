11d2f44879e69f695d0129b18800ae861ee34be2
jhphon0730 <eeung025@gmail.com>
2025-02-13 10:08:45 +0900

    feat : 다이러리 목록을 조회할 수 있는 함수 추가

--- front/src/lib/api/diary/index.ts
+++ front/src/lib/api/diary/index.ts
@@ -1,6 +1,6 @@
 import { FetchWithAuth, Response } from "@/lib/api"
 
-import { Diary } from "@/types/diary"
+import { Diary, DiaryViewType } from "@/types/diary"
 
 // 커플이 서로 가장 마지막에 작성한 다이어리를 가져옴
 type GetLatestDiaryRequest = {
@@ -11,6 +11,27 @@ type GetLatestDiaryResponse = {
 }
 export const GetLatestDiary = async ({coupleID}: GetLatestDiaryRequest): Promise<Response<GetLatestDiaryResponse>> => {
 	const res = await FetchWithAuth(`/diaries/latest?coupleID=${coupleID}`, {
+		method: "GET",
+	})
+	return {
+		data: res.data,
+		state: res.state,
+		message: res.message,
+		error: res.error,
+	}
+}
+
+/** 다이어리 목록을 조회
+ * 사용자가 작성한 다이어리, 커플이 작성한 다이어리, 커플과 사용자가 함께 작성한 다이어리를 모두 조회
+ */
+type GetDiariesRequest = {
+	DiaryViewType: DiaryViewType
+}
+type GetDiariesResponse = {
+	diaries: Diary[]
+}
+export const GetDiaries = async ({DiaryViewType}: GetDiariesRequest): Promise<Response<GetDiariesResponse>> => {
+	const res = await FetchWithAuth(`/diaries/all?category=${DiaryViewType}`, {
 		method: "GET",
 	})
 	return {
